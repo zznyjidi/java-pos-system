0d217ef6682f874a0c052a5f002f04df4d766b3f
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2026-01-12 09:05:47 -0500

    feat: add detailed parsing for Operation Result

--- src/exception/FileCorruptedException.java
+++ src/exception/FileCorruptedException.java
@@ -2,5 +2,17 @@ package exception;
 
 import java.io.IOException;
 
+import objects.Reservation;
+
 public class FileCorruptedException extends IOException {
+    Reservation conflictReservation;
+
+    public FileCorruptedException(Reservation conflict) {
+        super();
+        this.conflictReservation = conflict;
+    }
+
+    public Reservation getConflictReservation() {
+        return conflictReservation;
+    }
 }
